fe6dd75eb4bc8b8e4c4c8f5c9b50399eba9e6fe0
llosinskas <lucas_losinskas@hotmail.com>
2025-07-18 18:18:49 -0300

    Adicionar

--- NewProject.py
+++ NewProject.py
@@ -9,6 +9,7 @@ import FreeCADGui as Gui
 import FreeCAD as App
 import Spreadsheet
 from UI.insert_circuit import InsertCircuit
+import WorkbenchBase as WB
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -43,23 +44,27 @@ class newProjetctEletrical:
     def GetResources(self):
         return {'Pixmap': os.path.join(os.path.dirname(os.path.abspath(__file__)),"Resources/Icons", 'newProject.svg'), 'MenuText': "Novo Projeto", 'ToolTip':"Iniciar um novo projeto"}
 
-
 class CreateModel:
 
     def Activated(self):
-        doc = App.newDocument("Model")
-        if doc is None:
-            App.Console.PrintError('No active document found.\n')
-            return 
-
-        folder = doc.addObject("App::DocumentObjectGroup", "Simbolo 2D")
-        folder = doc.addObject("App::DocumentObjectGroup", "Foto")
-        folder = doc.addObject("App::DocumentObjectGroup", "Modelo 3D")
-        folder = doc.addObject("App::DocumentObjectGroup", "Descricao")
-        folder = doc.addObject("App::DocumentObjectGroup", "Detalhes 2D")
+        pasta_destino = f"{WB.__dir__}/Componentes/Eletrica"    
+        if not os.path.exists(pasta_destino):
+            os.makedirs(pasta_destino)
         
+        nome, ok = QtGui.QInputDialog.getText(None, "Salvar desenho", "Digite o nome do arquivo:")
         
-        
+        if ok and nome.strip():
+            caminho = os.path.join(pasta_destino, nome.strip()+".FCStd")
+            doc = App.newDocument(nome.strip())
+            folder = doc.addObject("App::DocumentObjectGroup", "Simbolo 2D")
+            folder = doc.addObject("App::DocumentObjectGroup", "Foto")
+            folder = doc.addObject("App::DocumentObjectGroup", "Modelo 3D")
+            folder = doc.addObject("App::DocumentObjectGroup", "Descricao")
+            folder = doc.addObject("App::DocumentObjectGroup", "Detalhes 2D")
+            App.ActiveDocument.saveAs(caminho)
+            
+        else:
+            QtGui.QMessageBox.warning(None, "Cancelado", "Operação cancelada")
                 
         doc.recompute()
 
@@ -67,8 +72,6 @@ class CreateModel:
         return {
             'Pixmap': os.path.join(os.path.dirname(os.path.abspath(__file__)),"Resources/Icons", 'add_model.svg'), 
             'MenuText': "Criar Model", 'ToolTip':"Botão para criar Models"}
-
-
 
 class newSpreadsheet:
     def Activated(self):
@@ -99,7 +102,6 @@ class newSpreadsheet:
 
 class addCircuit(object):
     
-    
     def Activated(self):
         form = InsertCircuit()
    
@@ -124,8 +126,63 @@ class addCircuit(object):
             'ToolTip':"Adicionar os circuitos na planilha"}
 
 class listElements:
+
+    def is_descendant(child, parent):
+        if child == parent:
+            return True
+        if hasattr(child, "Group"):
+            for obj in child.Group:
+                if listElements.is_descendant(obj, parent):
+                    return True
+        return False
+    
+
     def Activated(self):
-        pass
+        pasta_destino = f"{WB.__dir__}/Componentes/Eletrica"    
+        if not os.path.exists(pasta_destino):
+            os.makedirs(pasta_destino)
+        else:
+            arquivos = [f for f in os.listdir(pasta_destino) if f.endswith(".FCStd")]
+            if not arquivos:
+                QtGui.QMessageBox.information(None, "Nenhum arquivo", "Não há arquivos FCStd na pasta.")
+            else:
+                item, ok = QtGui.QInputDialog.getItem(None, "Abrir desenho", "Escolha o arquivo:", arquivos, editable=False)
+
+                if ok:
+
+                    doc = App.ActiveDocument
+                    if doc is None:
+                        App.Console.PrintError('No active document found.\n')
+                        return 
+                    caminho = os.path.join(pasta_destino, item)
+                    # App.openDocument(caminho)
+                    item_sem = item.split(".")[0]
+                    folder = doc.addObject("App::DocumentObjectGroup", item_sem)
+                    doc2 = App.open(caminho)
+                    
+                    for obj in doc2.Objects:
+                        copy = doc.copyObject(obj)
+                        
+                        for 
+                        if not listElements.is_descendant(obj, folder):    
+                            parent = copy.getParentGroup()
+                            folder.addObject(copy)
+                        # if hasattr(copy, 'getParentGroup'):
+                            
+                        #     if not listElements.is_descendant(obj, folder):
+                        #         parent = copy.getParentGroup()
+                            
+                        #     if parent:
+                        #         parent.removeObject(copy)                     
+                        
+                    App.closeDocument(item_sem)
+                    doc.recompute()
+
+                    # doc.mergeProject(caminho)
+                    
+                    
+                    
+        
     def GetResources(self):
         return {
             'Pixmap': os.path.join(os.path.dirname(os.path.abspath(__file__)),"Resources/Icons", 'listElements.svg'), 
